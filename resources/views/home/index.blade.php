55217741e839df26702d1f4999028c0393f31267
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-07-22 20:59:51 +0300

    Feature

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,13 +3,30 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
+        <title>Home</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <h1>Home Page</h1>
+        <nav>
+            <label>Joshic Web</label>
+
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+                <li>
+                    <a href="">Register</a>
+                </li>
+                <li>
+                    <a href="">Login</a>
+                </li>
+            </ul>
+        <nav>
         <script src="" async defer></script>
     </body>
 </html>
