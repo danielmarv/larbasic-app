cec60bb2d0a529b8a656a495db1de5b68d0b772f
Daniel Ntege <danientege785@gmail.com>
2023-08-04 18:35:05 +0300

    commit

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,6 +6,9 @@
         <title>Home</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
@@ -20,13 +23,14 @@
                     <a href="">Contact</a>
                 </li>
                 <li>
-                    <a href="">Register</a>
+                    <a href="{{ route('register') }}" class="btn btn-success">Register</a>
                 </li>
                 <li>
-                    <a href="">Login</a>
+                    <a href="{{ route('login ') }}" class="btn btn-primary">Login</a>
                 </li>
             </ul>
         <nav>
         <script src="" async defer></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
 </html>
